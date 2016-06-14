76d1dc64f2c9e789bd6641c8aa492a97cc57920d
Alec <hsu022210@gmail.com>
2016-06-14 12:51:15 +0800

    echart

--- www/js/hana_echart.js
+++ www/js/hana_echart.js
@@ -3,88 +3,95 @@ var myChart = echarts.init(document.getElementById('DataChart'));
 
 // 指定图表的配置项和数据
 var option = {
-        title : {
-            text: '年/月',
-            textStyle: {
-                fontSize: 12,
-                color: '#ffffff',
-            },
-            // subtext: '数据来自网络',
+    color: ['#ffffff','#124846'],
+    title : {
+        text: '年/月',
+        textStyle: {
+            fontSize: 12,
+            color: '#ffffff',
+            fontWeight: 'normal',
         },
-        tooltip : {
-            trigger: 'axis'
+        // subtext: '数据来自网络',
+    },
+    tooltip : {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['毛利率',],
+        textStyle: {
+            fontSize: 12,
+            color: '#ffffff',
+            fontWeight: 'normal',
         },
-        legend: {
-            data:['毛利率',],
-            textStyle: {
-                color: '#ffffff',
-            },
-            itemWidth: 55,
-            itemHeight: 22,
-            backgroundColor: '#1d2338'
-        },
-        toolbox: {
-            show : false,
-            feature : {
-                mark : {show: true},
-                dataView : {show: true, readOnly: false},
-                magicType: {show: true, type: ['line', 'bar']},
-                restore : {show: true},
-                saveAsImage : {show: true}
-            }
-        },
-        calculable : true,
-        grid: {
-            borderColor: '#000000',
-        },
-        xAxis : [
-            {
-                type : 'value',
-                boundaryGap : [0, 0.01],
-                axisLabel:{
-                    textStyle: {
-                        color: '#ffffff',
-                    }
-                },
-                axisTick:{
-                    show: false,
-                },
-                axisLine:{
-                    show: false,
-                },
-                splitLine:{
-                    show: false,
+        itemWidth: 55,
+        itemHeight: 22,
+        backgroundColor: '#1d2338'
+    },
+    toolbox: {
+        show : false,
+        feature : {
+            mark : {show: true},
+            dataView : {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar']},
+            restore : {show: true},
+            saveAsImage : {show: true}
+        }
+    },
+    calculable : true,
+    grid: {
+        borderColor: '#000000',
+    },
+    xAxis : [
+        {
+            type : 'value',
+            boundaryGap : [0, 0.01],
+            axisLabel:{
+                textStyle: {
+                    color: '#ffffff',
                 }
-            }
-        ],
-        yAxis : [
-            {
-                type : 'category',
-                data : ['巴西','印尼','巴西','印尼','美国','巴西','印尼','美国','印度','中国'],
-                axisLabel:{
-                    textStyle: {
-                        color: '#ffffff',
-                    }
-                },
-                axisTick:{
-                    show: false,
-                },
-                axisLine:{
-                    show: false,
-                },
-                splitLine:{
-                    show: false,
-                }
-            }
-        ],
-        series : [
-            {
-                name:'毛利率',
-                type:'bar',
-                data:[18203, 23489, 18203, 23489, 29034, 18203, 23489, 29034, 104970, 131744]
             },
-        ]
-    };
+            axisTick:{
+                show: false,
+            },
+            axisLine:{
+                show: false,
+            },
+            splitLine:{
+                show: false,
+            }
+        }
+    ],
+    yAxis : [
+        {
+            type : 'category',
+            data : ['2015/8','2015/9','2015/10','2015/11','2015/12','2016/1','2016/2','2016/3','2016/4','2016/5'],
+            axisLabel:{
+                textStyle: {
+                    color: '#ffffff',
+                    fontSize: 12,
+                },
+                interval: 0,
+                margin: 4,
+            },
+            axisTick:{
+                show: false,
+            },
+            axisLine:{
+                show: false,
+            },
+            splitLine:{
+                show: false,
+            }
+        }
+    ],
+    series : [
+        {
+            name:'毛利率',
+            type:'bar',
+            data:[18203, 23489, 18203, 23489, 29034, 18203, 23489, 29034, 104970, 131744]
+        },
+    ]
+};
 
 // 使用刚指定的配置项和数据显示图表。
 myChart.setOption(option);
