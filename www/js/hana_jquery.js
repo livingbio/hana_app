ccf3df318ccb1aab25019aa8d65055d9ee6a2524
Alec <hsu022210@gmail.com>
2016-06-14 15:12:15 +0800

    profile sidebar

--- www/js/hana_jquery.js
+++ www/js/hana_jquery.js
@@ -1,15 +1,9 @@
 function main() {
-    // $('.sideBar').hide();
     $('.Toggle-sideBar').on('click', function(){
-        if( $(".sideBar").css("margin-left") =='0px' ) {
-            $('.sideBar').animate({ 'margin-left': '-270px' }, 'slow', function(){
-                // $('.sideBar').hide();
-            });
+        if( $('body').css("margin-left") =='270px' ) {
             $('body').animate({ 'margin-left': '0px', 'margin-right': '0px' }, 'slow');
         }
         else {
-            // $('.sideBar').show();
-            $('.sideBar').animate({ 'margin-left': '0px' }, 'slow');
             $('body').animate({ 'margin-left': '270px', 'margin-right': '-270px' }, 'slow');
         }
     });
