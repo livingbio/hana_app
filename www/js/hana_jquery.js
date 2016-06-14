7ec5c9aaf5ad1de8bd3551894d1583a85caa532c
Alec <hsu022210@gmail.com>
2016-06-14 15:54:47 +0800

    doing filter ShowHide

--- www/js/hana_jquery.js
+++ www/js/hana_jquery.js
@@ -7,6 +7,17 @@ function main() {
             $('body').animate({ 'margin-left': '270px', 'margin-right': '-270px' }, 'slow');
         }
     });
+
+    $('#filter').hide();
+
+    $('.Toggle-filter').click(function() {
+        if ($('#filter').is(':visible')) {
+            $('#filter').hide();
+        }else{
+            $('#filter').show();
+        }
+    });
+
 }
 
 $(document).ready(main);
