106b4740abb688dd78f7a8131f1fe19499eec01b
Alec <hsu022210@gmail.com>
2016-06-24 16:50:31 +0800

    speed up sidebar animation

--- www/js/hana_jquery.js
+++ www/js/hana_jquery.js
@@ -2,10 +2,10 @@ function main() {
 
     $(document).on('click', '.Toggle-sideBar', function() {
         if( $('body').css("margin-left") =='250px' ) {
-            $('body').animate({ 'margin-left': '0px', 'margin-right': '0px' }, 200);
+            $('body').animate({ 'margin-left': '0px', 'margin-right': '0px' }, 100);
         }
         else {
-            $('body').animate({ 'margin-left': '250px', 'margin-right': '-250px' }, 200);
+            $('body').animate({ 'margin-left': '250px', 'margin-right': '-250px' }, 100);
         }
     });
 
